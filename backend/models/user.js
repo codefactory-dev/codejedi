25fd76e632862290f1b41d9b98519e3d1fc59cc6
Roberta Mota <roberta.cabralmota@ucalgary.ca>
2020-07-08 15:46:17 -0400

    update user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,15 +1,90 @@
+const Comment = require('../models/comment');
+const Rating = require('../models/rating');
+const QTrack = require('../models/qtrack');
+const QBasic = require('../models/qbasic');
 const mongoose = require('mongoose');
 
-const User = mongoose.model('User',{
-    
-    name: {
+
+const userSchema = new mongoose.Schema({
+    firstname: {
         type: String,
-        required: true,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
         trim: true,
-        lowercase: true
+        lowercase: true,
+        required: true
+    },
+    username: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        required: true,
+    },
+    joinDate: {
+        type: Date,
+        default: Date.now
+    },
+    profileImage: {
+        type: Buffer,
+        default: undefined
+    },
+    profileVisibility: {
+        type: Boolean,
+        required: true
+    },
+    ratingTds: [{ 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Rating"
+    }],
+    commentIds: [{ 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Comment"
+    }],
+    questionIds: [{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "QBasic"
+    }],
+    qTrackIds: [{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "QTrack"
+    }],
+    qTrackSummary: {
+        nbTracks: {
+            type: Number,
+            default: 0
+        },
+        nbTracksPerType: {
+            type: Map,
+            of: Number,
+            required: true
+        },
+        avgDuration: {
+            type: Number,
+            default: 0
+        },
+        avgDurationPerType: {
+            type: Map,
+            of: Number,
+            required: true
+        },
+        nbPDifficultyPerType: {
+            type: Map,
+            of: Number,
+            required: true
+        }
     }
+});
 
-    
-})
-
-module.exports = User
+module.exports = mongoose.model("User", userSchema);
