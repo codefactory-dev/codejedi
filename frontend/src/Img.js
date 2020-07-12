59aa6ac5232d0f0e5b6410bea4d127108c79ec53
Roberta Mota <roberta.cabralmota@ucalgary.ca>
2020-07-12 12:24:44 -0400

    update Img.js compoent to handle data returned from the server

--- frontend/src/Img.js
+++ frontend/src/Img.js
@@ -1,43 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 function Img() { 
-    const [imgs, setImgs] = useState(null);
+    const [img, setImg] = useState('');
+    let imgFile = undefined;
 
     useEffect(()=> {
         // GET request
         async function getImgs() {
-            const fetchImgs = await axios.get('/profilepics'); 
+            const fetchImgs = await axios.get('/.netlify/functions/server/api/profilepics'); 
         
             if (fetchImgs) {
-                // console.log(fetchImgs.data); 
-                setImgs(fetchImgs.data);
+                // TODO
             }
         }
-        
-        getImgs();
     }, []);
+
+    const arrayBufferToBase64 = (buffer) => {
+        var binary = '';
+        var bytes = [].slice.call(new Uint8Array(buffer));
+        bytes.forEach((b) => binary += String.fromCharCode(b));
+        return window.btoa(binary);
+    };
+
+    const handleSubmit = evt => {
+        evt.preventDefault();
+        uploadImage();
+
+        async function uploadImage() {
+            var formData = new FormData();
+
+            formData.append('profilepic', imgFile);
+            //console.log('>> formData >> ', formData);
+
+            const result = await axios.post('/.netlify/functions/server/api/profilepics', formData, {
+                                                    headers: { 'Content-Type': 'multipart/form-data'} });
+
+            // console.log(result.data);
+            // console.log(arrayBufferToBase64(result.data.buffer.data))
+            let im = 'data:image/jpeg;base64,' + result.data.postImgBase64;
+            setImg(im);
+        }
+    }
 
     // --------------------------------------------------------------------
     // RENDERING
     // --------------------------------------------------------------------
 
-    const renderImgs = () => {
-        return (
-            imgs.map((img, idx) => <img key={idx} src={`data(${img})`} />)
-        );
-    };
-
     return (
         <React.Fragment>
-            <form action="/profilepics" encType="multipart/form-data" method="POST"> 
-                    <input type="file" name="profilepic" />
+            <form encType="multipart/form-data" onSubmit={handleSubmit}> 
+                    <input type="file" name="profilepic" onChange={e => imgFile = e.target.files.files[0]}/>
                     <input type="submit" value="Upload a file"/>
             </form>
-            {imgs ? renderImgs() : undefined}
+            {img ? <img
+                    src={img}
+                    alt='Helpful alt text'/> : undefined}
         </React.Fragment>
     );
-
 };
 
 export default Img;
