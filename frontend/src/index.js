60b763ba1410f2bd2ba963027dbbd061ea1e9971
Roberta Mota <roberta.cabralmota@ucalgary.ca>
2020-07-14 19:58:03 -0400

    - add frontend dependency: codemirror - add codeEditor component. dummy, temporary component for testing codemirror   languages currently used: javascript, java (default), c++   user can write code and send post request to the server   server returns code, code editor is updated with received code and turns readonly

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import Img from './Img';
+import CodeEditor from './CodeEditor';
 
-ReactDOM.render(<React.StrictMode><Img /></React.StrictMode>, document.getElementById('root'));
+ReactDOM.render(<React.StrictMode><CodeEditor /></React.StrictMode>, document.getElementById('root'));
