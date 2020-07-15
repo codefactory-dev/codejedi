acd0dd09a6ea7b73ceaf329243055d08bc7bc186
Guilherme Reis <grrbm2@gmail.com>
2020-07-15 17:14:05 -0300

    added function to compile in the backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import Img from './Img';
-import CodeEditor from './CodeEditor';
+import App from './App.js';
 
-ReactDOM.render(<React.StrictMode><CodeEditor /></React.StrictMode>, document.getElementById('root'));
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
