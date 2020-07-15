cbcfe17a4cbf7c9ad0efeb0784cc3e38b62ee2da
Guilherme Reis <grrbm2@gmail.com>
2020-07-15 18:45:36 -0300

    created readFileSync function

--- frontend/src/utils/ConvertCodeToOneLiner.js
+++ frontend/src/utils/ConvertCodeToOneLiner.js
@@ -1,3 +1,21 @@
+function readTextFileSync()
+{
+    // Make sure we got a filename on the command line.
+    if (process.argv.length < 3) {
+        console.log('Usage: node ' + process.argv[1] + ' FILENAME');
+        process.exit(1);
+    }
+    try {  
+        var fs = require('fs')
+        , filename = process.argv[2];
+        var data = fs.readFileSync(filename, 'utf8');
+        console.log(data.toString());
+        return data;    
+    } catch(e) {
+        console.log('Error:', e.stack);
+    }
+}
+
 function readTextFile()
 {
     // Make sure we got a filename on the command line.
@@ -14,4 +32,10 @@ function readTextFile()
         console.log(data)
     });
 }
-readTextFile();
+function ConvertCodeToOneLiner()
+{
+    var text = readTextFileSync();
+    text.replace(/\n|\t/g, ' ');
+    console.log(text);
+}
+ConvertCodeToOneLiner();
