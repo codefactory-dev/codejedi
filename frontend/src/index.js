c4a142bbb3d9279560e141d6f337bc481aea6476
Guilherme Reis <grrbm2@gmail.com>
2020-07-15 17:20:27 -0300

    fixed double import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import Img from './Img';
-import App from './App.js';
+//import CodeEditor from './CodeEditor';
 
 ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
