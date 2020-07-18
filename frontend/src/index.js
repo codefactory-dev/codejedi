6e19d642439a5ae650dc1163c32b63388e5f4c6f
Guilherme Reis <grrbm2@gmail.com>
2020-07-18 14:16:57 -0300

    created EditorTestcases.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import Img from './Img';
-//import CodeEditor from './CodeEditor';
+import CodeEditor from './CodeEditor';
 
-ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
+ReactDOM.render(<React.StrictMode><CodeEditor /></React.StrictMode>, document.getElementById('root'));
