f377c2135520f16e6b3ca33e7b5a60077b10c67b
Roberta Mota <roberta.cabralmota@ucalgary.ca>
2020-07-22 15:39:49 -0400

    update rating.test

--- backend/tests/rating.test.js
+++ backend/tests/rating.test.js
@@ -2,6 +2,35 @@ const mongoose = require('mongoose'),
       request = require('supertest'),
       app = require('../app');
 
+const User = require('../models/user');
+
+const userOneId = new mongoose.Types.ObjectId;
+const userOne = {
+      _id: userOneId,
+      firstname: 'Roberta',
+      lastname: 'Mota',
+      email: 'roberta.cmota@gmail.com',
+      username: 'roberta.crmota',
+      password: 'roberta.crmota123',
+      qTrackSummary: {
+          nbTracksPerType: {
+              array: 0,
+              string: 0,
+              graph: 0
+          },
+          avgDurationPerType: {
+              array: 0,
+              string: 0,
+              graph: 0
+          },
+          nbPDifficultyPerType: {
+              array: 0,
+              string: 0,
+              graph: 0
+          }
+      }
+};
+
 describe('Ratings routes', () => {
   let connection;
   let db;
@@ -19,15 +48,52 @@ describe('Ratings routes', () => {
 
   afterAll(async () => mongoose.disconnect());
 
+  // create test user (creator)
+  beforeEach(async() => {
+      await User.deleteMany({});
+      await new User(userOne).save();
+  });
 
-  it('should be able to get ratings', async () => {
+
+  // ----------------------------------------------------------------------------
+  // TEST CASES - POST /ratings 
+  // ----------------------------------------------------------------------------
+
+  it('should be able to post a rating', async () => {
+  });
+
+  it('shoudl failt to post a rating without a creator', async () => {
     const response = await request(app)
-      .get('/ratings')
-      .send({
-        title: "Dummy sample test",
-        description: "This is just a dummy request to test ratings request"
-      })
+                              .post('/ratings')
+                              .send({
+                                creatorId: userOneId,
+                                value: 3
+                              });
 
     expect(response.status).toBe(200);
   });
+
+  it('shoudl failt to post a rating without a value', async () => {
+  });
+
+
+  // ----------------------------------------------------------------------------
+  // TEST CASES - GET /ratings/:id GET
+  // ----------------------------------------------------------------------------
+
+
+  // ----------------------------------------------------------------------------
+  // TEST CASES - PUT /ratings/:id/edit 
+  // ----------------------------------------------------------------------------
+
+  // it('should be able to get ratings', async () => {
+  //   const response = await request(app)
+  //     .get('/ratings')
+  //     .send({
+  //       title: "Dummy sample test",
+  //       description: "This is just a dummy request to test ratings request"
+  //     })
+
+  //   expect(response.status).toBe(200);
+  // });
 });
