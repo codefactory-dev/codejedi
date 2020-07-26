782ff9a270df90be59526fa6c1ae906a52f28600
Roberta Mota <roberta.cabralmota@ucalgary.ca>
2020-07-26 10:03:46 -0400

    add ./router/utils.js - include utility functions to be used in both router and test files

--- backend/routers/utils.js
+++ backend/routers/utils.js
@@ -1,3 +1,26 @@
+/** 
+    Function to check whether any of the given parameters is null
+
+    @param  {...*}      values - 1+ values to check if null 
+    @return {boolean}
+*/
 const isNull = (...values) => values.reduce((acc, v) => acc || v == null, false); 
 
-module.exports = { isNull }
+// -----------------------------------------------------------------------------
+// Rating
+// -----------------------------------------------------------------------------
+
+/** 
+    Function to recompute the average rating given a newly added rating
+
+    @param  {QBasic} question - question with a newly added rating
+    @param  {number} newValue - newly added rating's value 
+    @return {number} updated average rating
+*/
+const calcAvgRating = (question, newValue) => {
+    const prevAvgRating = question.avgRatings || 0;
+    const prevNbRating = question.nbRatings || 0;
+    return ((prevAvgRating*prevNbRating) + newValue) / (prevNbRating+1);
+}
+
+module.exports = { isNull, calcAvgRating }
