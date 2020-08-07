2558644fc10469d63c54b80569c189423dd6838f
Guilherme Reis <grrbm2@gmail.com>
2020-08-07 18:14:37 -0300

    finished adding try/catches

--- backend/tests/comment.test.js
+++ backend/tests/comment.test.js
@@ -44,20 +44,16 @@ describe('Comment routes', () => {
             qbasics.push(q.basic); 
             qdetails.push(q.detail);
       });      
-      console.log("gonna create qbasics with "+JSON.stringify(qbasics));
 
       for(let i=0;i<qbasics.length;i++)
       {
         var qbasic = new QBasic(qbasics[i]);
         try{
-          console.log('before save');
           let savedQbasic = await qbasic.save(); //when fail its goes to catch
-          console.log(savedQbasic); //when success it print.
-          console.log('after save');
         }
         catch(e)
         {
-          console.log("ERROR SAVING qBasic i="+i+": "+e.toString());
+          console.log("Error saving qBasic"+e.toString());
         }
       }
 
@@ -65,39 +61,40 @@ describe('Comment routes', () => {
       {
         var qdetail = new QDetail(qdetails[i]);
         try{
-          console.log('before save');
           let savedQdetail = await qdetail.save(); //when fail its goes to catch
-          console.log(savedQdetail); //when success it print.
-          console.log('after save');
         }
         catch(e)
         {
-          console.log("ERROR SAVING qDetail i="+i+": "+e.toString());
+          console.log("Error saving qDetail"+e.toString());
         }
       }
 
-      
-      await User.create(users);
-      const seedComments = generateComments(7, users, seedQuestions);     
-      const comments = await Comment.create(seedComments);
-
-      console.log("these are all the comments: "+JSON.stringify(comments));
+      var comments;
+      try{
+        await User.create(users);
+        const seedComments = generateComments(7, users, seedQuestions);     
+        comments = await Comment.create(seedComments);
+      } catch(e){
+        console.log("Error creating seed comments: "+e.toString());
+      }
 
       for(let i=0;i<comments.length;i++)
       {
-        var comment = comments[i];
-        const user = await User.findById(comment.creatorId);
-        user.commentIds.addToSet(comment._id);
-        await user.save();
+        try{
+          var comment = comments[i];
+          const user = await User.findById(comment.creatorId);
+          user.commentIds.addToSet(comment._id);
+          await user.save();
+        } catch(e){
+          console.log("Error adding commentIds to user");
+        }
       }
 
 
       try{
         const allQbasics = await QBasic.find({});
-        console.log("these are all the qbasics: "+JSON.stringify(allQbasics));
-      } catch(e)
-      {
-        console.log("ERROR FETCHING QBASICS: "+e.toString());
+      } catch(e){
+        console.log("Error fetching qBasics: "+e.toString());
       }
 
       for (const comment of comments) {
@@ -106,27 +103,13 @@ describe('Comment routes', () => {
           var qBasicCommentBelongsTo = await QBasic.findById(comment.questionId);
           var qDetailCommentBelongsTo = await QDetail.findById(qBasicCommentBelongsTo.detailsId);
           await qDetailCommentBelongsTo.commentIds.addToSet(comment._id);
+          console.log("ADDED comment Id "+comment._id+" to qDetails "+qDetailCommentBelongsTo._id);
           await qDetailCommentBelongsTo.save();
-        } catch(e)
-        {
-          console.log("ERROR: "+e.toString());
+        } catch(e){
+          console.log("Error: "+e.toString());
         }
       }
-      console.log("GOT HEEEEEEERE");
-
-      /*
-      comments.map(comment => {
-        //Do somethign with the comment
-        var qBasicCommentBelongsTo = await QBasic.findById(comment.questionId);
-        var qDetailCommentBelongsTo = await QDetail.findById(qBasicCommentBelongsTo.detailsId);
-        console.log("qBasicCommentBelongsTo: "+qBasicCommentBelongsTo._id);
-        console.log("qDetailCommentBelongsTo: "+qDetailCommentBelongsTo._id);
-        console.log("qDetailCommentBelongsTo.commentIds: "+JSON.stringify(qDetailCommentBelongsTo.commentIds));
-        qDetailCommentBelongsTo.commentIds.addToSet(comment._id);
-        comment.save();
-      })
-      */
-      
+      console.log("Finished");
   });
 
 
