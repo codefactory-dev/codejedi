7c816f9f75424780afe6b9f266190a69cc7c44a1
Guilherme Reis <grrbm2@gmail.com>
2020-08-16 20:30:46 -0300

    added material ui navbar

--- frontend/src/Navbar/Navbar.js
+++ frontend/src/Navbar/Navbar.js
@@ -1,16 +1,43 @@
 import React from 'react';
-
-import './Navbar.scss';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
+//import './Navbar.scss';
 
 function NavBar() { 
-   
+    const classes = useStyles();
+    
 
     return (
-        <div id="navbar">
-            Navbar
-        </div> 
+        <AppBar position="static">
+        <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+            News
+            </Typography>
+            <Button color="inherit">Login</Button>
+        </Toolbar>
+        </AppBar>
     );
 
 }
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+}));
 
 export default NavBar;
