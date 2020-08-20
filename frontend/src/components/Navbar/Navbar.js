6881eebc9a63b7a694cdb27d9c4dc3bd95b5f662
Guilherme Reis <grrbm2@gmail.com>
2020-08-20 18:50:06 -0300

    added avatar placeholder

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -7,8 +7,10 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import MenuIcon from '@material-ui/icons/Menu';
 import SvgIcon from '@material-ui/core/SvgIcon';
+import Avatar from '@material-ui/core/Avatar';
 //import './Navbar.scss';
 import { ReactComponent as YodaLogo } from '../../imgs/Yoda Logo.svg';
+import placeholderAvatar from '../../imgs/profile pic.png'
 
 function NavBar() { 
     const classes = useStyles();
@@ -23,11 +25,11 @@ function NavBar() {
             <SvgIcon component={YodaLogo} style={{ fontSize: 30, verticalAlign: 'middle' }} viewBox="0 0 42 42" />
             <span style={{marginLeft: '8px'}}>CODE</span>
             <span style={{color:`${yodaGreen}`}}> JEDI</span>
+            <span style={{marginLeft: '8px'}}>BROWSE</span>
+            <span style={{marginLeft: '8px'}}>PROFILE</span>
             </Typography>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                <MenuIcon />
-            </IconButton>
-            <Button color="inherit">Login</Button>
+            <Avatar alt="Remy Sharp" src={placeholderAvatar} />
+            <Button color="inherit">roberta.cmota</Button>
         </Toolbar>
         </AppBar>
     );
