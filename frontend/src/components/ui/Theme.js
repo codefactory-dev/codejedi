0ac08a8b77472a8c5340baf4e060bb70d803ea98
Roberta Mota <roberta.cabralmota@ucalgary.ca>
2020-09-17 20:23:32 -0400

    update theme.js -- add common component styling

--- frontend/src/components/ui/Theme.js
+++ frontend/src/components/ui/Theme.js
@@ -4,6 +4,15 @@ const mainBlack = "#1B1C1D";
 const mainGrey = "#F4F4F4";
 const yodaGreen = '#D7E2C6';
 
+const teal = '#00B5AD';
+const salmon = '#FF6565';
+
+const black1 = '#262626';
+const black2 = '#313233';
+const black3 = '#3C3C3C';
+
+const greyLight = '#F2F2F2';
+
 export default createMuiTheme({
     logo: {
         yoda: `${yodaGreen}`
@@ -12,12 +21,35 @@ export default createMuiTheme({
         common: {
             black: `${mainBlack}`,
             grey: `${mainGrey}`,
+
+            black1: `${black1}`,
+            black2: `${black2}`,
+            black3: `${black3}`,
+            greyLight: `${greyLight}`
         },
         primary: {
-            main: `${mainBlack}`
+            main: `${teal}`
         },
         secondary: {
-            main: `${mainGrey}`
+            main: `${salmon}`
         }
+    },
+
+    // -------------------------------------------
+    // components
+    // -------------------------------------------
+
+    btnPrimaryOutline: {
+        color: `${teal}`,
+        borderColor: `${teal}`,
+        textTransform: 'none',
+        fontSize: '.9rem',
+        fontWeight: '200',
+    },
+    btnPrimaryText: {
+        color: `${teal}`,
+        fontSize: '.9rem',
+        textTransform: 'none',
+        fontWeight: '200',
     }
 })
