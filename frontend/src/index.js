2b3abf4eb888c0c82d7e11f5dd8b9efd7b8a4f1e
Guilherme Reis <grrbm2@gmail.com>
2020-10-07 12:10:53 -0300

    adjusted correctly page containers

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import QuestionPage from './pages/QuestionPage/QuestionPage.js'
 import SigninPage from './pages/SigninPage/SigninPage.js'
+//import SignInSide from './pages/SignInSide/SignInSide.js'
 import { ThemeProvider } from '@material-ui/styles'
 import theme from './components/Ui/Theme.js'
 import CssBaseline from '@material-ui/core/CssBaseline'
-
+//<SignInSide />
+//<SigninPage>
 ReactDOM.render(
                 <React.StrictMode>
                     <CssBaseline />
--- frontend/src/pages/SigninPage/SigninPage.js
+++ frontend/src/pages/SigninPage/SigninPage.js
@@ -10,10 +10,47 @@ import TextField from '@material-ui/core/TextField'
 import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
 import SvgIcon from '@material-ui/core/SvgIcon';
-import { ReactComponent as Illustration } from '../../imgs/illustration.svg'
+import Illustration from '../../imgs/illustration.svg'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+
 import Hidden from '@material-ui/core/Hidden';
 import PropTypes from 'prop-types';
 import withWidth from '@material-ui/core/withWidth';
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        height: '100vh'        
+    },
+    image: {
+        backgroundImage: `url(${Illustration})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundColor:'#1B1C1D',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    },
+    initialPage: {
+        backgroundColor:'#1B1C1D',
+    },
+    paper: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor:'#1B1C1D',
+        color:'white'
+    },
+    codejediLogo: {
+        backgroundColor:'#1B1C1D',
+        color:'white',
+        backgroundSize: 'cover',
+    },
+    sectionDesktop: {
+        [theme.breakpoints.down('sm')]: {
+          display: 'none',
+        },
+    },
+}));
+
 
 function SigninPage() {
     const classes = useStyles();
@@ -34,30 +71,11 @@ function SigninPage() {
 
     },[])
     return (
-        <Grid
-            container
-            direction="row"
-            justify="center"
-            alignItems="center"
-        >
-            <Grid 
-                className={classes.sectionDesktop}
-                item
-                >
-                <SvgIcon 
-                    component={Illustration}
-                    style={{ fontSize: 500, verticalAlign: 'middle' }} 
-                    viewBox="0 0 960 1080" 
-                    className={classes.initialPage}
-                />
-            </Grid>
-            <Grid 
-                item
-                direction="column"
-                justify="center"
-                alignItems="center"
-                className={classes.initialPage}
-            >
+        <Grid container className={classes.container}>
+            <CssBaseline />
+            <Grid item xs={false} sm={4} md={7} className={classes.image}/>
+            <Grid item item xs={12} sm={8} md={5} component={Paper} elevation={6} className={classes.initialPage} square>
+                <div className={classes.paper}>
                     <Typography variant="h3">SIGN IN</Typography>
                     <Typography variant="subtitle2" gutterBottom>Welcome back ! Please login to your account.</Typography>
 
@@ -77,34 +95,15 @@ function SigninPage() {
                         Login
                     </Button>
 
-                    <h2>Forgot your password? Reset</h2>
-                    <h2>Don't have an account? Sign Up</h2>
 
-
-                    <h3>Term of use. Privacy policy</h3>
+                    <Typography variant="subtitle">Forgot your password? Reset</Typography>
+                    <Typography variant="subtitle">Don't have an account? Sign Up</Typography>
+                    <Typography variant="subtitle">Term of use. Privacy policy</Typography>
+                </div>
             </Grid>
         </Grid>
     )
 
 }
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > *': {
-          margin: theme.spacing(1),
-          width: '25ch'
-        },
-    },
-    initialPage: {
-        height: '900px',
-        backgroundColor:'#1B1C1D',
-        color:'white'
-    },
-    sectionDesktop: {
-        [theme.breakpoints.down('sm')]: {
-          display: 'none',
-        },
-    },
-}));
 
 export default SigninPage;
