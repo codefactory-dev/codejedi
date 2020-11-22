e30b1ae488eeffde828e022923a79608b413066f
crmotaroberta <roberta.cmota@gmail.com>
2020-11-22 06:39:12 -0500

    started descriptionPage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,18 +8,18 @@ import QuestionPageProto from './pages/QuestionPageProto/QuestionPageProto.js'
 //import InitialPage from './pages/InitialPage/InitialPage.js'
 import SwipeList from './components/List/SwipeList';
 import TestInputList from './components/List/TestInputList';
-import ParameterInputList from './components/List/ParameterInputList';
 import { ThemeProvider } from '@material-ui/styles'
 import theme from './components/ui/Theme'
 import masterCss from './masterCss.scss'
 //import SignInSide from './pages/SignInSide/SignInSide.js'
 import SigninPage from './pages/SigninPage/SigninPage.js'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import DescriptionPage from './pages/DescriptionPage/DescriptionPage';
 
 ReactDOM.render(
                 <React.StrictMode>
                     <ThemeProvider theme={theme}>
-                        <ParameterInputList />
+                        <DescriptionPage />
                     </ThemeProvider>
                 </React.StrictMode>, 
                 document.getElementById('root'));
---
+++ frontend/src/pages/DescriptionPage/DescriptionPage.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect } from 'react';
+import ParameterInputList from '../../components/List/ParameterInputList';
+
+export default function DescriptionPage() {
+
+    return (
+        <div>
+            <ParameterInputList />
+        </div>
+    );
+
+}
