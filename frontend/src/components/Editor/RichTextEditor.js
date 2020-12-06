7e326607fe0aeb2dc88f4280096bf85856c81471
Guilherme Reis <grrbm2@gmail.com>
2020-12-06 11:59:38 -0300

    converted rich text editor to a functional component

--- frontend/src/components/Editor/RichTextEditor.js
+++ frontend/src/components/Editor/RichTextEditor.js
@@ -1,70 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { Editor, EditorState, RichUtils } from "draft-js";
 
 //more examples: https://codesandbox.io/examples/package/draft-js
 
-class RichTextEditor extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			editorState: EditorState.createEmpty()
-		};
-	}
+function RichTextEditor() {
 
-	onChange = editorState => {
-		this.setState({
-			editorState
-		});
+  const [editorState,setEditorState] = useState(EditorState.createEmpty());
+
+	const onChange = editorState => {
+		setEditorState(editorState)
 	};
 
-	handleKeyCommand = command => {
+	const handleKeyCommand = command => {
 		const newState = RichUtils.handleKeyCommand(
-			this.state.editorState,
+			editorState,
 			command
 		);
 		if (newState) {
-			this.onChange(newState);
+			onChange(newState);
 			return "handled";
 		}
 		return "not-handled";
 	};
 
-	onUnderlineClick = () => {
-		this.onChange(
-			RichUtils.toggleInlineStyle(this.state.editorState, "UNDERLINE")
+	const onUnderlineClick = () => {
+		onChange(
+			RichUtils.toggleInlineStyle(editorState, "UNDERLINE")
 		);
 	};
 
-	onBoldClick = () => {
-		this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, "BOLD"));
+	const onBoldClick = () => {
+		onChange(RichUtils.toggleInlineStyle(editorState, "BOLD"));
 	};
 
-	onItalicClick = () => {
-		this.onChange(
-			RichUtils.toggleInlineStyle(this.state.editorState, "ITALIC")
+	const onItalicClick = () => {
+		onChange(
+			RichUtils.toggleInlineStyle(editorState, "ITALIC")
 		);
 	};
 
-	render() {
-		return (
-			<div className="editorContainer">
-				<button onClick={this.onUnderlineClick}>U</button>
-				<button onClick={this.onBoldClick}>
-					<b>B</b>
-				</button>
-				<button onClick={this.onItalicClick}>
-					<em>I</em>
-				</button>
-				<div className="editors">
-					<Editor
-						editorState={this.state.editorState}
-						handleKeyCommand={this.handleKeyCommand}
-						onChange={this.onChange}
-					/>
-				</div>
-			</div>
-		);
-	}
+  return (
+    <div className="editorContainer">
+      <button onClick={onUnderlineClick}>U</button>
+      <button onClick={onBoldClick}>
+        <b>B</b>
+      </button>
+      <button onClick={onItalicClick}>
+        <em>I</em>
+      </button>
+      <div className="editors">
+        <Editor
+          editorState={editorState}
+          handleKeyCommand={handleKeyCommand}
+          onChange={onChange}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default RichTextEditor;
