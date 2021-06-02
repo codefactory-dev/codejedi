defd8c5907dd702020bd970d8af6ebbe32ec7eb1
Guilherme Reis <grrbm2@gmail.com>
2021-06-01 23:09:35 -0300

    corrected error logging in

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -15,7 +15,7 @@ const fetchClient = () => {
   // Set the AUTH token for any request
   instance.interceptors.request.use(function (config) {
     const tokens = localStorage.getItem('tokens');
-    if (tokens !== 'undefined'){
+    if (tokens && tokens !== 'undefined'){
       const parsed = JSON.parse(tokens)
       config.headers.Authorization =  parsed.token ? `Bearer ${parsed.token}` : '';
     }
