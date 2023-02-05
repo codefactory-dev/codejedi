c5ecc9d444f0e486a77110102375b684efbe4ca7
Guilherme Reis <grrbm2@gmail.com>
2023-02-05 20:40:57 -0300

    finished utility to connect to the database

--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -1,29 +1,40 @@
-const express = require('express'),
-      router = express.Router({mergeParams: true}),
-      User = require('../models/user'),
-      utils = require('../src/utils/utils'),
-      jwt = require('jsonwebtoken'),
-      middleware = require('../middleware/index'),
-      sendConfirmationEmail = require('../templates/signupConfirmationEmail.js')
+const express = require("express"),
+  router = express.Router({ mergeParams: true }),
+  User = require("../models/user"),
+  utils = require("../src/utils/utils"),
+  jwt = require("jsonwebtoken"),
+  middleware = require("../middleware/index"),
+  sendConfirmationEmail = require("../templates/signupConfirmationEmail.js"),
+  { checkDatabaseActive } = require("./utils");
 
 // validate the user credentials
-router.post('/auth/signin', async function (req, res) {
-  try{
-    const user = await User.findByCredentials(req.body.email, req.body.password);
+router.post("/auth/signin", async function (req, res) {
+  try {
+    const databaseIsActive = await checkDatabaseActive();
+    console.log("database is active: " + databaseIsActive);
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
     const token = await user.generateAuthToken();
-    console.log("returning user and token. user: "+JSON.stringify(user)+". token: "+JSON.stringify(token));
-    res.status(200).send({user, token});
-  } catch(error){
-    console.log("Error signing in. "+error.message)
+    console.log(
+      "returning user and token. user: " +
+        JSON.stringify(user) +
+        ". token: " +
+        JSON.stringify(token)
+    );
+    res.status(200).send({ user, token });
+  } catch (error) {
+    console.log("Error signing in. " + error.message);
     res.status(400).send(error.toString());
-  }   
+  }
 });
 
-router.post('/auth/signout', middleware.auth, async function (req, res) {
+router.post("/auth/signout", middleware.auth, async function (req, res) {
   try {
-    req.user.tokens = req.user.tokens.filter((token)=>{
-      return token.token !== req.token
-    })
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
     await req.user.save();
 
     res.send();
@@ -32,7 +43,7 @@ router.post('/auth/signout', middleware.auth, async function (req, res) {
   }
 });
 
-router.post('/auth/signoutall', middleware.auth, async function (req, res) {
+router.post("/auth/signoutall", middleware.auth, async function (req, res) {
   try {
     req.user.tokens = [];
     await req.user.save();
@@ -43,46 +54,45 @@ router.post('/auth/signoutall', middleware.auth, async function (req, res) {
   }
 });
 
-
 //user confirmation by token
-router.post('/auth/validate/:token', async (req,res) => {
+router.post("/auth/validate/:token", async (req, res) => {
   try {
     const tk = req.params.token;
-    const decoded = await jwt.verify(tk,process.env.JWT_SECRET);
+    const decoded = await jwt.verify(tk, process.env.JWT_SECRET);
     const user = await User.findById(decoded._id);
-    const token = user.tokens.find((e) => { e.token === req.params.token });
-    if (!token)
-    {
+    const token = user.tokens.find((e) => {
+      e.token === req.params.token;
+    });
+    if (!token) {
       return res.status(404).send("Your token has expired.");
     }
-    if (token.validated)
-    {
+    if (token.validated) {
       user.set({ validated: true });
       await user.save();
       return res.status(200).send(user);
     }
-  } catch(e){
-    return res.status(500).send("Error: "+e.toString())
+  } catch (e) {
+    return res.status(500).send("Error: " + e.toString());
   }
-
-
 });
 
 //request validation by token
-router.post('/auth/requestValidation', async (req,res) => {
-    console.log(`REQUEST :: validate user ${req.body.email}`);    
-    
-    //generate token and send it with validated=true encoded
-    //save token to the user
-    try{
-      const user = await User.findByCredentials(req.body.email,req.body.password);
-      const validationToken = await user.generateValidationToken();
-      const result = await sendConfirmationEmail(token, user.email);
-      res.status(200).send("Email sent.");
-    }catch(e){
-      res.status(500).send("Error: "+e.toString());
-    }
-    
-})
+router.post("/auth/requestValidation", async (req, res) => {
+  console.log(`REQUEST :: validate user ${req.body.email}`);
+
+  //generate token and send it with validated=true encoded
+  //save token to the user
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const validationToken = await user.generateValidationToken();
+    const result = await sendConfirmationEmail(token, user.email);
+    res.status(200).send("Email sent.");
+  } catch (e) {
+    res.status(500).send("Error: " + e.toString());
+  }
+});
 
 module.exports = router;
--- backend/routers/utils.js
+++ backend/routers/utils.js
@@ -1,10 +1,24 @@
+const MongoClient = require("mongodb").MongoClient;
 /** 
     Function to check whether any of the given parameters is null
 
     @param  {...*}      values - 1+ values to check if null 
     @return {boolean}
 */
-const isNull = (...values) => values.reduce((acc, v) => acc || v == null, false); 
+const isNull = (...values) =>
+  values.reduce((acc, v) => acc || v == null, false);
 
+const checkDatabaseActive = async () => {
+  try {
+    const uri = process.env.MONGODB_URL;
+    const client = await MongoClient.connect(uri, { useNewUrlParser: true });
+    console.log("Connected successfully to server");
+    client.close();
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
 
-module.exports = { isNull }
+module.exports = { isNull, checkDatabaseActive };
